beeb0921a0057498d66c3eedd9b26dd9a932edc8
Mudather7 <mudatherabdo00@gmail.com>
2023-11-02 08:33:00 -0700

    if opp != '+' or opp != '-' or opp != '*' or opp != '/':

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,18 +12,20 @@ if __name__ = "__main__":
         exit(1)
 
     opp = argv[2]
-    a = int(argv[1])
-    b = int(argv[3])
-
-    if opp == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif opp == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif opp == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif opp == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
 
     if opp != '+' or opp != '-' or opp != '*' or opp != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if (opp == '+'):
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif (opp == '-'):
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif (opp == '*'):
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif (opp == '/'):
+        print("{} / {} = {}".format(a, b, div(a, b)))
+
