fa9029fe6af026e73d0dd617ba01a4135a8d23e9
Mudather7 <mudatherabdo00@gmail.com>
2023-11-04 06:16:12 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if my_list == 0:
+        return None
+
+    biggest = 0
+
+    for i in my_list:
+
+        if i > biggest:
+            biggest = i
+
+    return biggest
