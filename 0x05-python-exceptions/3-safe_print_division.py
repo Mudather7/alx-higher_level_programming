dc2f1a9693237df9c32502996a10e5253cea75ff
Mudather7 <mudatherabdo00@gmail.com>
2023-11-20 06:11:10 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+
+    try:
+        result = a / d
+
+    except ZeroDivisionError:
+
+        pass
+
+    finally:
+
+        print("Inside result: {}".format(result))
+
+    return result
