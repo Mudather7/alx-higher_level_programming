2ec6a2baa2781711a9341b72e18f4d75831ffeaf
Mudather7 <mudatherabdo00@gmail.com>
2023-11-21 07:14:24 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''Defines a class square.'''
+"""Defines a class square."""
 
 
 class square:
-    '''Represent a square. No body.'''
+    """Represent a square. No body."""
     pass
