1fbb2f05947e320ee93071568c68aa5eda40c830
Mudather7 <mudatherabdo00@gmail.com>
2023-11-27 07:57:39 -0800

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,8 @@
 class Rectangle:
     '''Representation of a Rectangle'''
 
+    number_of_instance = 0
+
     def __init__(self, width=0, height=0):
         '''Initializes the rectangle'''
         self.height = height
