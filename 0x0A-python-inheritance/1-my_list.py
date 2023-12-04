bb39b9e6d238d2df82db3deda9f84ef0f2166036
Mudather7 <mudatherabdo00@gmail.com>
2023-12-04 05:07:10 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module for MYlist class.'''
+
+
+def MYlist(list):
+    '''Custom MYlist class.'''
+    def print_sorted(self):
+        '''Method for printing sorted list'''
+
+        return (sorted(self))
