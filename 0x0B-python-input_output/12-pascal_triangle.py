4badd6624f8126ed44295c2f0024115cf1162ec6
Mudather7 <mudatherabdo00@gmail.com>
2023-12-05 12:33:40 -0800

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''defineing pascal_triangle function'''
+
+
+def pascal_triangle(n):
+    '''returns a list of lists of integers representing the Pascal’s triangle'''
+
+    array = [[1]]
+
+    while len(array) != n:
+        tri = array[-1]
+        tmp = [1]
+
+        for x in range(len(tri) - 1):
+            tmp.append(tri[x] + tri[x + 1]
+        tmp.append(1)
+        array.append(tmp)
+
+        return array
