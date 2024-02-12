f214fd5ba4cae3359c3403af70d6a7d730cba23c
Mudather7 <mudatherabdo00@gmail.com>
2024-02-12 07:54:37 -0800

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = process.argv;
+const number = parseInt(argv[2]);
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${number}`);
+}
