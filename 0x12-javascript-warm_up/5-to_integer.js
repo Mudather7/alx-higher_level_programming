886963990a06179500e6dfed747bf539fe49ceb4
Mudather Abdo <138704178+Mudather7@users.noreply.github.com>
2024-04-22 00:57:03 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const argv = process.argv;
-const number = parseInt(argv[2]);
-
-if (isNaN(number)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${number}`);
-}
