3f2971aab9739130a5d17aae799c0018ab93f051
Mudather7 <mudatherabdo00@gmail.com>
2024-04-23 08:10:56 -0700

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 const Square = require('/5-square');
 
 class Square1 extends Square {
-  charprint (c) {
+  charPrint (c) {
     if (!c) this.print();
     else {
       for (let i = 0; i < this.width; i++) console.log(c.repeat(this.height));
