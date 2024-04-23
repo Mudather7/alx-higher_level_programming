ee6f01e96cdfef24528c98903bb2dbc68d20ecb8
Mudather7 <mudatherabdo00@gmail.com>
2024-04-23 07:30:11 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,12 +9,11 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      const row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'X ';
-      }
-      console.log(row);
+      const row = [];
+      for (let j = 0; j < this.width; j++) row.push('X');
+      console.log(`${row.join('')}`);
     }
   }
 }
+
 module.exports = Rectangle;
