2116a397e3358af882e0f6b2aa961570503a1ecc
Mudather7 <mudatherabdo00@gmail.com>
2024-04-28 08:56:07 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,10 +10,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name\
-                FROM cities LEFT JOIN states\
-                ON states.id = cities.state_id\
-                ORDER BY cities.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
