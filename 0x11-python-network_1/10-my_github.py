7ac9dd33cb7ee4cc16e1c82816f5f6c0c41ba7eb
Mudather7 <mudatherabdo00@gmail.com>
2024-05-10 04:09:04 -0700

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -19,7 +19,7 @@ def main(argv):
     try:
         profile_info = response.json()
         print(profile_info['id'])
-    if response.json == {}:
+    if profile_info == {}:
         print('None')
 
 
